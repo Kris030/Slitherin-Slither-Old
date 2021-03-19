fd22e5337d6060f623f454afaba445270b6908f2
Kristóf Bácskai <kristof.bacskai03@gmail.com>
2021-03-19 15:33:21 +0100

    create new bot token + add config.ts to .gitignore

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const config = {
-	token: 'Nzc3ODk2NTM4MDg5MTkzNDgy.X7KGmw.yTRkqRKMUPnEt_VTy_GxmKq4FPw',
+	token: 'Nzc3ODk2NTM4MDg5MTkzNDgy.X7KGmw.88gn9fKiFIqhoCfvwbh-7TubrK8',
 	ownerID: '437253014168797194',
 	admins: [
 		
