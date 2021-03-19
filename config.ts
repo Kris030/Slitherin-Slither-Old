c115b641b57f285175264705fa1d22e0a5e981df
the boi <45177246+Kris030@users.noreply.github.com>
2021-03-19 15:37:19 +0100

    Delete config.ts
    token lol #2

--- config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-	token: 'Nzc3ODk2NTM4MDg5MTkzNDgy.X7KGmw.88gn9fKiFIqhoCfvwbh-7TubrK8',
-	ownerID: '437253014168797194',
-	admins: [
-		
-	]
-};
-Object.freeze(config);
-export default config;
